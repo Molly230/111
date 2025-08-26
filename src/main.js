f045b9afac5eae4ee776d40d9fe76b8853f6ef0f
Molly230 <luying20001@outlook.com>
2025-08-26 22:04:02 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
+  import { createApp } from 'vue'
+  import { createPinia } from 'pinia'
+  import ElementPlus from 'element-plus'
+  import 'element-plus/dist/index.css'
+  import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+  import router from './router'
+  import App from './App.vue'
 
+  const app = createApp(App)
+  const pinia = createPinia()
